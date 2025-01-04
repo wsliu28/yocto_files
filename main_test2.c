c7717ff983e3ded55da9a15ce401ba19aa5a94fd
wsliu28 <neonflashing@yahoo.com>
2025-01-04 20:11:17 +0000

    RL

--- main_test2.c
+++ main_test2.c
@@ -6,7 +6,7 @@ extern void print_out(char *in_string);
 
 int main()
 {
-    printf("Please input what you say\n");
+    printf("Please input whatttt I say\n");
     scanf("%s", in_sentance);
     print_out(in_sentance);
    
