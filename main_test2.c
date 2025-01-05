9dcf428bae941e3060e0102d2cd4074946155a23
wsliu28 <52434752+wsliu28@users.noreply.github.com>
2025-01-05 12:42:46 +0000

    Update main_test2.c

--- main_test2.c
+++ main_test2.c
@@ -6,7 +6,7 @@ extern void print_out(char *in_string);
 
 int main()
 {
-    printf("Please input whatttt I say\n");
+    printf("Please input whatttt she says\n");
     scanf("%s", in_sentance);
     print_out(in_sentance);
    
